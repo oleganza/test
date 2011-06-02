8b8771e8b75d513a6a6cb4a06a4c6aee87a56b70
Oleg Andreev <oleganza@gmail.com>
2011-06-02 16:23:13 +0200

    nfp e 2k

---
+++ gen.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+1000.times {
+  name = "noise_" + rand(2**128).to_s(16) + ".txt"
+  File.open(name, "w"){|f| f.write(rand(2**10000).to_s(2)) }
+  puts "Added #{name}"
+}
+
+system("git add . && git commit -m '#{rand(2**rand(40)).to_s(32)} #{rand(2**rand(40)).to_s(32)} #{rand(2**rand(40)).to_s(32)}'")
+  
